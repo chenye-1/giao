fe3f0f72ab63e924f178710f377728802c97b579
cy <1085274803@qq.com>
2021-02-01 18:25:29 +0800

    index

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,4 +1,17 @@
-$.ajaxPrefilter(function(option){
-    option.url="http://ajax.frontend.itheima.net"+option.url
+$.ajaxPrefilter(function (option) {
+    option.url = "http://ajax.frontend.itheima.net" + option.url
 
+    if (option.url.includes('/my/')) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    option.complete = function (res) {
+        console.log(res);
+        if (res.responseJSON.status == 1){
+            localStorage.removeItem('token')
+            location.href='/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$(function () {
+    // 请求数据
+    getUserInfo()
+})
+function getUserInfo() {
+    $.ajax({
+        type: 'get',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || '',
+        // },
+        success(res) {
+            console.log(res);
+            if (res.status !== 0) return layui.layer.msg('获取数据失败')
+            renderAvatar(res.data)
+
+        }
+    })
+}
+// 渲染数据
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('.welcome').html('欢迎&nbsp;&nbsp' + name);
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.textAvatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.textAvatar').html(first).show()
+    }
+}
+
+// 退出
+var layer = layui.layer
+$('#logout').on('click', function () {
+    layer.confirm('你确定要退出吗', { icon: 3, title: '提示' }, function (index) {
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+
+    });
+})
